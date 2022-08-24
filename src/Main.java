2e7e658ca4108fc4c6fd5d699e8200e53fb69887
Stephany Xesquevixos <stexesquevixos@gmail.com>
2022-08-24 16:33:25 -0300

    Alteração no método deletedNumber para encontrar todos os números removidos da lista de 1 a 100

--- src/Main.java
+++ src/Main.java
@@ -3,40 +3,72 @@ import java.util.List;
 
 public class Main {
 
-    private List<Integer> arrayNumbers = new ArrayList<Integer>();
+    private final List<Integer> arrayNumbers = new ArrayList<Integer>();
+    private final List<Integer> arrayNumbersDeleted = new ArrayList<Integer>();
     private int expectedSum = 0;
-    private int sum = 0;
+    private static int initialSizeArrayNumbers = 0;
 
     public static void main(String[] args) {
 
         Main main = new Main();
         main.addingNumbersToTheList();
         main.expectedSum();
-        main.removeNumberFromList(67);
+        main.removeNumberFromList(3);
+        main.removeNumberFromList(46);
+        main.removeNumberFromList(12);
+        main.removeNumberFromList(92);
+        main.removeNumberFromList(99);
+        main.removeNumberFromList(66);
         main.deletedNumber();
 
     }
 
     public void addingNumbersToTheList() {
-        for (int i = 1; i <= 100; i++) {
-            arrayNumbers.add(i);
+        for (int number = 1; number <= 100; number++) {
+            arrayNumbers.add(number);
         }
+        initialSizeArrayNumbers = arrayNumbers.size();
     }
 
     public void expectedSum() {
-        for (int i = 0; i < arrayNumbers.size(); i++) {
-            expectedSum += arrayNumbers.get(i);
-        }
+        expectedSum = arrayNumbers.size() * (arrayNumbers.size() + 1) / 2;
     }
 
-    public void removeNumberFromList(int number) {
-        arrayNumbers.remove(number-1);
+    public void removeNumberFromList(int numberToBeDeleted) {
+        for (int number = 0; number < arrayNumbers.size(); number++) {
+            if (arrayNumbers.get(number) == numberToBeDeleted) {
+                arrayNumbers.remove(arrayNumbers.get(number));
+            }
+        }
     }
 
     public void deletedNumber(){
-        for (int t = 0; t < arrayNumbers.size(); t++) {
-            sum += arrayNumbers.get(t);
+        int sum = arrayNumbers.size() * (arrayNumbers.size() + 1) / 2;
+
+        int positionInTheArrayNumbers = 0;
+        int number = 1;
+        int difference = expectedSum - sum;
+
+        while (difference > 0) {
+            if (positionInTheArrayNumbers < arrayNumbers.size() && number != arrayNumbers.get(positionInTheArrayNumbers)) {
+                arrayNumbersDeleted.add(number);
+                difference -= number;
+                number += 1;
+            } else if (positionInTheArrayNumbers < arrayNumbers.size() && number == arrayNumbers.get(positionInTheArrayNumbers)) {
+                positionInTheArrayNumbers += 1;
+                number += 1;
+            } else if (positionInTheArrayNumbers >= arrayNumbers.size()) {
+                if (arrayNumbers.size() + arrayNumbersDeleted.size() == initialSizeArrayNumbers) {
+                    break;
+                } else {
+                    arrayNumbersDeleted.add(number);
+                    difference -= number;
+                }
+                positionInTheArrayNumbers += 1;
+                number += 1;
+            }
         }
-        System.out.println("O número que falta na lista é: " + (expectedSum-sum));
+
+        System.out.println("Os números que faltam na lista são: " + arrayNumbersDeleted);
     }
 }
